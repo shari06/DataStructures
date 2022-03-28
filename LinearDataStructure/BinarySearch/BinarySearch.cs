0b5a3789179d1ce4bca8e898a646489dec70e2a5
Srihari Rajendran <98453672+shari06@users.noreply.github.com>
2022-03-28 14:44:45 -0400

    Binary search issue fix

--- LinearDataStructure/BinarySearch/BinarySearch.cs
+++ LinearDataStructure/BinarySearch/BinarySearch.cs
@@ -23,39 +23,41 @@ namespace LinearDataStructure.BinarySearch
         public int BinarySearching(int[] arr, int key)
         {
             // Sort the give array in to ascending order
-            return BinarySearching(arr, key, 0, arr.Length);
+            return BinarySearching(arr, key, 0, arr.Length - 1);
         }
 
         private int BinarySearching(int[] arr, int key, int i, int l)
         {
             var result = -1;
-
-            if (i == l)
+            if (i <= l)
             {
-                if (arr[i] == key)
+                if (i == l)
                 {
-                    result = i;
+                    if (arr[i] == key)
+                    {
+                        result = i;
+                    }
                 }
-            }
-            else
-            {
-                // To get the median form array i to l
-                var m = (i + l) / 2;
-                if (key == arr[m])
+                else
                 {
-                    result = m;
-                }
-                // Search Key was present in the left side from median 
-                else if (arr[m] > key)
-                {
-                    result = BinarySearching(arr, key, i, m);
-                }
-                // Search Key was present in the Right side from median 
-                else if (arr[m] < key)
-                {
-                    result = BinarySearching(arr, key, m, l);
-                }
+                    // To get the median form array i to l
+                    var m = l + (i - l) / 2;
+                    if (key == arr[m])
+                    {
+                        result = m;
+                    }
+                    // Search Key was present in the left side from median 
+                    else if (arr[m] > key)
+                    {
+                        result = BinarySearching(arr, key, i, m - 1);
+                    }
+                    // Search Key was present in the Right side from median 
+                    else if (arr[m] < key)
+                    {
+                        result = BinarySearching(arr, key, m + 1, l);
+                    }
 
+                }
             }
             return result;
         }
